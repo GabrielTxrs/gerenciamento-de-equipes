cb89c7f298b2afbe301c6c393a18387bd1b808b3
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2024-11-27 22:12:45 -0300

    Criei a tela de Login da aplicação
    On branch main
    new file:   Front-End/.editorconfig
    new file:   Front-End/.gitignore
    new file:   Front-End/.vscode/extensions.json
    new file:   Front-End/.vscode/launch.json
    new file:   Front-End/.vscode/tasks.json
    new file:   Front-End/README.md
    new file:   Front-End/angular.json
    new file:   Front-End/package-lock.json
    new file:   Front-End/package.json
    new file:   Front-End/public/favicon.ico
    new file:   Front-End/src/app/app.component.css
    new file:   Front-End/src/app/app.component.html
    new file:   Front-End/src/app/app.component.ts
    new file:   Front-End/src/app/app.config.ts
    new file:   Front-End/src/app/app.routes.ts
    new file:   Front-End/src/app/components/login/login.component.css
    new file:   Front-End/src/app/components/login/login.component.html
    new file:   Front-End/src/app/components/login/login.component.ts
    new file:   Front-End/src/app/services/login.service.ts
    new file:   Front-End/src/index.html
    new file:   Front-End/src/main.ts
    new file:   Front-End/src/styles.css
    new file:   Front-End/tsconfig.app.json
    new file:   Front-End/tsconfig.json
    modified:   README.md

---
+++ Front-End/src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginService } from '../../services/login.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  // loginService = inject(LoginService);
+  
+  constructor
+  (
+    private formBuilder: FormBuilder,
+    private loginService: LoginService)
+    {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  public loginUsuario() {
+    const dadosLogin = {
+      email: this.loginForm.get('email')?.value,
+      senha: this.loginForm.get('senha')?.value
+    }
+    console.log(dadosLogin);
+    this.loginService.loginUsuario(dadosLogin);
+  }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gerenciamento-de-equipes
 Projeto desenvolvido durante o processo seletivo da Softeam
+
+Para rodar o Front
+```
+$ npm install
+
+$ ng serve
+```
+
